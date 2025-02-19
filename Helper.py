6d8130091493830f8d6c3463233cf5346d0cfeaf
Foreverthelight <fashjosh2004@gmail.com>
2025-02-19 16:34:45 +0100

    Added Custom Function (addition, subtraction, floor division, factorial, modulus, max, min, exponent and camparision Functions)

---
+++ Helper.py
@@ -0,0 +1,36 @@
+#Addition Function
+def Sum_Function(a:float ,b:float )-> float: 
+    return a+b
+
+#Subtraction Function 
+def Sub_Function(a:float ,b:float ) -> float: 
+    return a-b
+
+#Floor Division Function
+def Floor_Division(a:int ,b:int ) -> int:
+    return a//b
+
+#Factorial Function 
+import math
+def Factorial_Function(Num: int )-> int:
+    return (math.factorial(Num))
+
+#Modulus Function 
+def Modulus_Function(a:int ,b:int)-> int:
+    return a%b
+
+#Maximum Value Function
+def Max_Value_Function(a,b,c,d,e):
+    return(max(a,b,c,d,e))
+
+#Minimum Value Function
+def Min_Value_Function(a,b,c,d,e):
+    return(min(a,b,c,d,e))
+
+#Exponent Function
+def Exponent_Function(a:float ,b:float)-> float:
+    return(a**b)
+
+#Compare Function
+def Compare_Function(a,b)-> bool:
+    return a==b  
