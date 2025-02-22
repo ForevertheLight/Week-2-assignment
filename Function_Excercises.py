16a3ecaa64d36bb87167f41ee0289040dea54932
Foreverthelight <fashjosh2004@gmail.com>
2025-02-22 21:13:49 +0100

    Making My Codes More Descriptive

--- Function_Excercises.py
+++ Function_Excercises.py
@@ -13,41 +13,41 @@ SixthValue=List[5]
 
 #Multiply two values 
 Multiply=Helper.Multiply_Function(FirstValue,ThirdValue)
-print(Multiply)
+print('Multiplying', FirstValue, 'by',ThirdValue, '= ',Multiply)
 
 #Exponent two values
 Exponent=Helper.Exponent_Function(SecondValue,FourthValue)
-print(Exponent)
+print('Exponent of', SecondValue, 'and', FourthValue, '= ', Exponent)
 
 #Subtract two values 
 Subtract= Helper.Sub_Function(FifthValue,FourthValue)
-print(Subtract)
+print('Subtract',FifthValue, 'From', FourthValue, '= ',Subtract)
 
 #Floor Division of two values
 Floor=Helper.Floor_Division(SixthValue,SecondValue)
-print(Floor)
+print('Floor Division of',SixthValue,'and', SecondValue, '= ',Floor)
 
 #Modulus of two Values
 Modulus=Helper.Modulus_Function(SixthValue,SecondValue)
-print(Modulus)
+print('Modulus of', SixthValue, 'and', SecondValue, '=', Modulus)
 
 #Factorial of a value
 Factorial=Helper.Factorial_Function(FourthValue)
-print(Factorial)
+print('Factorial of', FourthValue, '=', Factorial)
 
 #Compare two values 
 Compare=Helper.Compare_Function(SecondValue,SixthValue)
-print(Compare)
+print(SecondValue, 'greater than', SixthValue,'is ', Compare)
 
 #Minimum of Five Values 
 Minimum=Helper.Min_Value_Function(FirstValue,SecondValue,ThirdValue,FourthValue,FirstValue)
-print(Minimum)
+print('The Minimum Value of', FirstValue,SecondValue,ThirdValue,FourthValue,FirstValue,'= ',Minimum)
 
 #Maximum of Five Values
 Maximum=Helper.Max_Value_Function(FirstValue,SixthValue,FourthValue,SecondValue,ThirdValue)
-print(Maximum)
+print('The Maximum Value of', FirstValue,SixthValue,FourthValue,SecondValue, ThirdValue, '=', Maximum)
 
 #Add two values
 Addition=Helper.Sum_Function(FifthValue,SixthValue)
-print(Addition)
+print('The Addition of',FifthValue,'and', SixthValue, '= ',Addition)
 
