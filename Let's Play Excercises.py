f743717756211dcc787f454caea99733c09df311
ForevertheLight <fashjosh2004@gmail.com>
2025-02-23 23:06:32 +0100

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch Phronesis
     Changes to be committed:
    	modified:   Let's Play Excercises.py

---
+++ Let's Play Excercises.py
@@ -0,0 +1,11 @@
+#The manager of XYZ Supermarket has approached you, a software developer, 
+#to design a discount system for their upcoming Christmas campaign. 
+#The system should apply discounts based on the following conditions 
+#using Python functions and if statements:
+# A 3% discount for sales greater than or equal to 5,000.
+# A 5% discount for sales greater than or equal to 8,000.
+# A 10% discount for sales greater than or equal to 10,000.
+# A 15% discount for sales greater than or equal to 50,000.
+
+
+
