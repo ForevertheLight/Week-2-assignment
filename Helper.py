f721afaf0b7c43c863c7981fe2919f7ab61afab3
ForevertheLight <fashjosh2004@gmail.com>
2025-02-24 01:35:06 +0100

    I added Discount Function to File Helper.py

--- Helper.py
+++ Helper.py
@@ -39,3 +39,20 @@ def Compare_Function(a,b)-> bool:
 def Multiply_Function(a: float,b: float)-> float:
     return(a*b)
 
+#Percentage Function
+def Percentage_Function(a,b,c=100):
+    return (a/c)*b
+
+#Discount Function
+def Discount_Function(Amount):
+    if Amount >=5000 and Amount <=7999:
+        return (Percentage_Function(3,Amount))
+    elif Amount >=8000 and Amount <=9999:
+        return (Percentage_Function(5,Amount))
+    elif Amount >=10000 and Amount <= 49999:
+        return (Percentage_Function(10,Amount))
+    elif Amount >=50000:
+        return (Percentage_Function(15,Amount))
+    else:
+        return False
+
