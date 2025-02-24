769e7bc3208159a713a73accf5413e12585bdbbe
ForevertheLight <fashjosh2004@gmail.com>
2025-02-24 01:26:07 +0100

    Adding Let's Play Excercise One to My Repo

--- Let's Play Excercises.py
+++ Let's Play Excercises.py
@@ -6,6 +6,10 @@
 # A 5% discount for sales greater than or equal to 8,000.
 # A 10% discount for sales greater than or equal to 10,000.
 # A 15% discount for sales greater than or equal to 50,000.
-
+import Helper 
+Amount=int(input('Enter Amount of Goods:'))
+print('Amount of Goods','is','equal to', Amount)
+Discount=Helper.Discount_Function(Amount)
+print('Your Discount is ',Discount)
 
 
